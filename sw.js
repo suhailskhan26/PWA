a54db34250bbbab9ab6359a622912ae1e2356bec
suhailskhan26 <139943724+suhailskhan26@users.noreply.github.com>
2024-03-28 12:20:59 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,43 @@
-// /sw.js
-var CACHE_NAME = 'my-site-cache-v1'
-var urlsToCache = [
+if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function () {
+            navigator.serviceWorker.register('/sw.js', { scope: "/" }).then(function (registration) {
+                // Registration was successful
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }, function (err) {
+                // registration failed :(
+                console.log('ServiceWorker registration failed: ', err);
+            });
+        })
+    }
+    
+    var CACHE_NAME = 'my-site-cache-v1';
+    var urlsToCache = [
     '/',
     'index.html',
-];
+    /* '/styles/main.css',
+    '/script/main.js' */
+    ];
+    self.addEventListener('install', function (event) {
+        // Perform install steps
+        event.waitUntil(
+            caches.open(CACHE_NAME)
+                .then(function (cache) {
+                    console.log('Opened cache');
+                    return cache.addAll(urlsToCache);
+                })
+        )
+    });
 
-
-self.addEventListener('install', (event) => {
-    const cacheKey = 'MyFancyCacheName_v1';
-  
-    event.waitUntil(caches.open(cacheKey).then((cache) => {
-      // Add all the assets in the array to the 'MyFancyCacheName_v1'
-      // `Cache` instance for later use.
-      return cache.addAll([
-        '/css/global.bc7b80b7.css',
-        '/css/home.fe5d0b23.css',
-        '/js/home.d3cc4ba4.js',
-        '/js/jquery.43ca4933.js'
-      ]);
-    }));
-  });
+    self.addEventListener('fetch', function(event) {
+        event.respondWith(
+          caches.match(event.request)
+            .then(function(response) {
+              // Cache hit - return response
+              if (response) {
+                return response;
+              }
+              return fetch(event.request);
+            }
+          )
+        );
+      });
